b9ca483120c68382f0174021dea1a4254ee2dd02
Junhyeok Seo <jun673012@gmail.com>
2023-12-27 21:17:24 +0900

    Create 2163_hyeok.java

---
+++ week1/sjh/2163_hyeok.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        bw.write(n * m - 1 + "");
+        bw.flush();
+        bw.close();
+    }
+}
