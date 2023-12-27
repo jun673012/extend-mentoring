fcd6b6bd3c2b283c1c1a5b9957a25f0c6df7ea64
Junhyeok Seo <jun673012@gmail.com>
2023-12-27 22:00:07 +0900

    Create 2914_hyeok.java

---
+++ week1/sjh/2914_hyeok.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int i = Integer.parseInt(st.nextToken());
+
+        System.out.print(a * (i - 1) + 1);
+    }
+}
