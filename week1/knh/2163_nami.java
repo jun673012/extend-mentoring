ac9b843b6ea46e6ce1ceaac20b4af55ab24575f8
Nami <145953587+skagml9665@users.noreply.github.com>
2023-12-30 00:37:05 +0900

    Create 2163_nami.java

---
+++ week1/knh/2163_nami.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+
+        int s = n*m - 1;
+        System.out.println(s);
+
+    }}
