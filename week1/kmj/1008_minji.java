6b62aaedfc9019bc9b60b6b940eaebcd75f677c6
xoxxi <139968832+xoxxi@users.noreply.github.com>
2023-12-29 21:34:20 +0900

    Create 1008_minji.java

---
+++ week1/kmj/1008_minji.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        double a, b;
+
+        a = input.nextInt();
+        b = input.nextInt();
+
+        System.out.println(a/b);
+    }
+}
