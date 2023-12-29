50ec20093e40b84fc6a232f2f62a5d1a5d723d53
xoxxi <139968832+xoxxi@users.noreply.github.com>
2023-12-29 21:40:52 +0900

    Create 10926_minji.java

---
+++ week1/kmj/10926_minji.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        String name;
+
+        name = input.next();
+
+        System.out.println(name + "??!");
+    }
+}
