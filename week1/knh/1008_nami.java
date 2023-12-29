c9ef517114ff5c6e2eb73088483bf2048ba0f480
Nami <145953587+skagml9665@users.noreply.github.com>
2023-12-30 00:34:23 +0900

    Create 1008_nami.java

---
+++ week1/knh/1008_nami.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        double x = scanner.nextInt();
+        double y = scanner.nextInt();
+
+        double result = x/y;
+        System.out.println(result);
+
+}}
