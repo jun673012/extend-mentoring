4e87eaadeef18f33236bbacd4365243620ad2a62
Nami <145953587+skagml9665@users.noreply.github.com>
2024-01-04 04:24:40 +0900

    Create 2444_nami.java

---
+++ week2/knh/2444_nami.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for(int i=0; i<n-1; i++){
+            for(int j=i; j<n-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<(i+1)*2-1; j++){
+                System.out.print("*");
+            }
+
+            System.out.print("\n");
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<(n-i)*2-1; j++){
+                System.out.print("*");
+            }
+
+            System.out.print("\n");
+        }
+
+    }}
