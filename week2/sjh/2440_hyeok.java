4ce5ec07ad318a0ca7adec7ea4e30f1fad308c81
Junhyeok Seo <jun673012@gmail.com>
2024-01-03 19:41:14 +0900

    Create 2440_hyeok.java

---
+++ week2/sjh/2440_hyeok.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            bw.write("*".repeat(n - i) + "\n");
+        }
+
+        bw.flush();
+    }
+}
