6b7411d74a1f34f157e475bbbdc07dd7898f830c
Nami <145953587+skagml9665@users.noreply.github.com>
2024-01-03 16:24:12 +0900

    Create 14681_nami.java

---
+++ week2/knh/14681_nami.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+        if (x>0){
+            if (y>0){
+                bw.write(1+"");
+            } else {
+                bw.write(4+"");
+            }
+        } else{
+            if (y>0){
+                bw.write(2+"");
+            } else {
+                bw.write(3+"");
+            }
+        }
+
+        bw.close();
+    }
+}
