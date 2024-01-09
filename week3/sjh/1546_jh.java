97d1c6ec5ee9b7ba4387fe0020356bbd01615237
Junhyeok Seo <jun673012@gmail.com>
2024-01-10 01:14:18 +0900

    Update 1546_jh.java

--- week3/sjh/1546_jh.java
+++ week3/sjh/1546_jh.java
@@ -27,11 +27,7 @@ public class Main {
         }
 
         for (int i = 0; i < n; i++) {
-            arr[i] = arr[i] / m * 100;
-        }
-
-        for (int i = 0; i < n; i++) {
-            sum += arr[i];
+            sum += arr[i] / m * 100;
         }
 
         bw.write(sum / n + "");
