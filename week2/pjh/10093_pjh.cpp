fb8b70827e543d45a5790e78c6280b3ce3360216
Park jae hyeon <89504861+jjujjuwo@users.noreply.github.com>
2024-01-09 17:11:29 +0900

    Update 10093_pjh.cpp

--- week2/pjh/10093_pjh.cpp
+++ week2/pjh/10093_pjh.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main(){
-    unsigned long long a,b;
+    unsigned long long a,b,i;
     cin>>a>>b;
-    cout<<b-a-1<<endl;
-    for (int i=1; i<b-a;i++){
-        cout<<a+i<<" ";
+    if(a==b){
+        cout<<0<<endl;
+        return 0;
     }
+    int j=b-a;
+    cout<<abs(j)-1<<endl;
+    for (i=1; i<abs(j);i++){
+        if(b>a)cout<<a+i<<" ";
+        if(a>b)cout<<b+i<<" ";
+    }
+    return 0;
 }
