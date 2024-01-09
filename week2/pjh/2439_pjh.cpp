bcfe0e9b4f2f1de39af553e4a5c93bd28e3b97c5
Park jae hyeon <89504861+jjujjuwo@users.noreply.github.com>
2024-01-09 18:19:40 +0900

    Create 2439_pjh.cpp

---
+++ week2/pjh/2439_pjh.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    for (int i=0; i<a; i++){
+        for(int k=0; k<=a-i-2; k++){
+            cout<<" ";
+        }
+        for (int j=0; j<=i; j++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+}
+
+
