1dcdb151b608fd256a23d580fe05742530957a1e
xoxxi <139968832+xoxxi@users.noreply.github.com>
2024-01-10 01:25:22 +0900

    Update 2490_minji.java

--- week2/kmj/2490_minji.java
+++ week2/kmj/2490_minji.java
@@ -1,1 +1,30 @@
-.
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int[] num = new int[4];
+
+        for(int i=0; i<3; i++){
+            int count = 0;
+            for(int j=0; j<4; j++){
+                num[j]=input.nextInt();
+                if(num[j]==1){
+                    count++;
+                }
+            }
+            if(count==0)
+                System.out.println("D");
+            else if(count==1)
+                System.out.println("C");
+            else if(count==2)
+                System.out.println("B");
+            else if(count==3)
+                System.out.println("A");
+            else if(count==4)
+                System.out.println("E");
+        }  
+        
+    }
+}.
