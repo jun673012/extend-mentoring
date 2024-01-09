f16cf79650f937549555501f82401eba0a14d849
xoxxi <139968832+xoxxi@users.noreply.github.com>
2024-01-09 18:41:05 +0900

    Create 2440_minji.java

---
+++ week2/kmj/2440_minji.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        int a = input.nextInt();
+        
+        for(int i=a; i>0; i--){
+            for(int j=0; j<i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
