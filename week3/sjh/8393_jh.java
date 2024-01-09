2c76fbf14a69f3e3642689dbe457c4cdfb5320e1
Junhyeok Seo <jun673012@gmail.com>
2024-01-09 23:41:22 +0900

    Create 8393_jh.java

---
+++ week3/sjh/8393_jh.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += i + 1;
+        }
+
+        bw.write(sum + "");
+        bw.close();
+
+    }
+}
