50d670e342af5c3efe2444390594fdf55479061c
Junhyeok Seo <jun673012@gmail.com>
2024-01-17 03:25:13 +0900

    Create 10810_jh.java

---
+++ week4/sjh/10810_jh.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            for (int j = a; j <= b; j++) {
+                arr[j-1] = c;
+            }
+        }
+
+        for (int i : arr) {
+            bw.write(i + " ");
+        }
+
+        bw.close();
+    }
+}
