6a77d16150367874f610dc202173b65b5cea369d
xoxxi <139968832+xoxxi@users.noreply.github.com>
2024-01-18 02:50:36 +0900

    Create 2743_minji.java

---
+++ week4/kmj/2743_minji.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String a = input.next();
+
+        System.out.print(a.length());
+    }
+}
