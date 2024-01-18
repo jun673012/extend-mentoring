fa9fa2c0f78f5f5afe3b1d1682cede8336d80131
Nami <145953587+skagml9665@users.noreply.github.com>
2024-01-18 21:36:42 +0900

    Create 11654_nami.java

---
+++ week4/knh/11654_nami.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        String n = st.nextToken();
+        char m = n.charAt(0);
+
+        System.out.print((int)m);
+    }
+}
