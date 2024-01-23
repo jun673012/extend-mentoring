0dff17031e4d7d02466d54c6d8854de9fc37b959
Park jae hyeon <89504861+jjujjuwo@users.noreply.github.com>
2024-01-23 18:15:19 +0900

    Create 2845_pjh.cpp

---
+++ week4/pjh/2845_pjh.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int x,y,s;
+    cin>>x>>y;
+    s=x*y;
+    int a,b,c,d,e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a-s<<" "<<b-s<<" "<<c-s<<" "<<d-s<<" "<<e-s;
+}
