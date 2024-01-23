d8583b4ad562f882eb4e93688e6e1104de8673b2
Park jae hyeon <89504861+jjujjuwo@users.noreply.github.com>
2024-01-23 17:31:10 +0900

    Create 11718_pjh.cpp

---
+++ week4/pjh/11718_pjh.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    for(int i=0; i<100; i++){
+        string str;
+        getline(cin, str);
+        cout << str << '\n';
+        
+    }
+}
