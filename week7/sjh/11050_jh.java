38f301314bb969c0e04fef3225e96abe8eda557b
Junhyeok Seo <jun673012@gmail.com>
2024-02-07 00:31:15 +0900

    Update 11050_jh.java

--- week7/sjh/11050_jh.java
+++ week7/sjh/11050_jh.java
@@ -13,16 +13,16 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        int[][] arr = new int[n+1][n+1];
+        int[][] arr = new int[n + 1][n + 1];
 
-        for(int i=0; i<=n; i++){
+        for(int i = 0; i <= n; i++){
             arr[i][0] = 1;
             arr[i][i] = 1;
         }
 
-        for(int i=2; i<=n; i++){
-            for(int j=1; j<i; j++){
-                arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
+        for(int i = 2; i <= n; i++){
+            for(int j = 1; j < i; j++){
+                arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
             }
         }
 
