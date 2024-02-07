2f86ddd9a5214406796c334d8ced539d5ac94772
xoxxi <139968832+xoxxi@users.noreply.github.com>
2024-02-07 11:40:06 +0900

    Create 2869_minji.java

---
+++ week7/kmj/2869_minji.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+
+        int days = (int)Math.ceil((double)(v-a)/(a-b));
+
+        System.out.print(days+1);
+    }
+}
