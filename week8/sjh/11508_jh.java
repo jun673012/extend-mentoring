cc00d4e0c1ebbf10ef7cf4f2fba487158f953986
Junhyeok Seo <jun673012@gmail.com>
2024-02-14 02:01:22 +0900

    Create 11508_jh.java

---
+++ week8/sjh/11508_jh.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        Integer[] arr = new Integer[n];
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        for (int i = 0; i < n; i++) {
+            if ((i + 1) % 3 == 0) {
+                continue;
+            }
+            sum += arr[i];
+        }
+
+        bw.write(sum + "");
+        bw.close();
+    }
+}
